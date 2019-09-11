b830553b8cf730a08398eeb2756edcbda6fb1b58
ARAjlgarber <55189103+ARAjlgarber@users.noreply.github.com>
2019-09-11 14:20:07 +0200

    Version 1

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -1,5 +1,18 @@
 /* Here, you should entry the required libraries */
+#include <stdio.h>
 
 void main(){
 	/* Here, you should write the source code of your program */
+	int A;
+	int B;
+	int C;
+	
+	printf("Select A");
+	scanf("%d", &A);
+	printf("Select B");
+	scanf("%d", &B);
+	C=A+B;
+	printf("The result is %d", C);
+	
+
 }
